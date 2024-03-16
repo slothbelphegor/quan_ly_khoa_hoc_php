ddc1631dca96a9fbe9c332933b6019d52b022d12
manle423 <trungmanle.vm@gmail.com>
2024-03-17 00:06:56 +0700

    change attr phone to username

--- classes/user.php
+++ classes/user.php
@@ -4,7 +4,7 @@ class User
     private $id;
     private $name;
     private $email;
-    private $phone;
+    private $username;
     private $address;
     private $password;
     private $is_active;
@@ -18,11 +18,11 @@ class User
     {
         $this->name = $name;
     }
-    public function __construct($name , $email, $phone, $address, $password, $is_active = 1, $role_id = 2)
+    public function __construct($name, $email, $username, $address, $password, $is_active = 1, $role_id = 2)
     {
         $this->name = $name;
         $this->email = $email;
-        $this->phone = $phone;
+        $this->username = $username;
         $this->address = $address;
         $this->password = $password;
         $this->is_active = $is_active;
@@ -30,7 +30,7 @@ class User
     }
     protected function validate()
     {
-        $rs = $this->name != '' && $this->email != '' && $this->phone != '' && $this->address != '' && $this->password != '';
+        $rs = $this->name != '' && $this->email != '' && $this->username != '' && $this->address != '' && $this->password != '';
         return $rs;
     }
     public static function authenticatebyemail($conn, $email, $password)
@@ -51,11 +51,11 @@ class User
         return false;
     }
 
-    public static function authenticatebyphone($conn, $phone, $password)
+    public static function authenticatebyusername($conn, $username, $password)
     {
-        $sql = "select * from users where phone = :phone";
+        $sql = "select * from users where username = :username";
         $stmt = $conn->prepare($sql);
-        $stmt->bindValue(":phone", $phone, PDO::PARAM_STR);
+        $stmt->bindValue(":username", $username, PDO::PARAM_STR);
         $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
         $stmt->execute();
         $user = $stmt->fetchObject();
@@ -71,7 +71,7 @@ class User
 
     public static function getUserInfo($conn, $id)
     {
-        $sql = "SELECT * FROM users WHERE id = :id";
+        $sql = "select * from users where id = :id";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
@@ -80,19 +80,106 @@ class User
         return $user;
     }
 
-    public function test($conn){
-        echo $this->role_id;
+    public static function getAllUserInfo($conn)
+    {
+        try {
+            $sql = "select u.id, u.name, u.email, u.username, u.address, u.is_active, u.role_id from users u;";
+            $stmt = $conn->prepare($sql);
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            if ($stmt->execute()) {
+                $users = $stmt->fetchAll();
+                return $users;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+
+    public static function deactiveUser($conn, $id)
+    {
+        try {
+            $sql = "update users set is_active = false where id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            $e->getMessage();
+            return false;
+        }
+    }
+    public static function activeUser($conn, $id)
+    {
+        try {
+            $sql = "update users set is_active = true where id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            $e->getMessage();
+            return false;
+        }
+    }
+
+    public static function isUserActive($conn, $identifier)
+    {
+        try {
+            $sql = "select is_active from users where email = :email OR username = :username";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':email', $identifier, PDO::PARAM_STR);
+            $stmt->bindValue(':username', $identifier, PDO::PARAM_STR);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $user && $user['is_active'] == 1;
+        } catch (PDOException $e) {
+            $e->getMessage();
+            return false;
+        }
+    }
+
+    public static function emailExists($conn, $email)
+    {
+        try {
+            $sql = "select count(*) as count from users where email = :email";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $result['count'] > 0;
+        } catch (PDOException $e) {
+            $e->getMessage();
+            return false;
+        }
+    }
+
+    public static function updatePassword($conn, $email, $newPassword)
+    {
+        try {
+            $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+            // Truy vấn cập nhật mật khẩu trong cơ sở dữ liệu
+            $sql = "update users set password = :password where email = :email";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(":password", $hashedPassword);
+            $stmt->bindValue(":email", $email);
+
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            $e->getMessage();
+            return false;
+        }
     }
 
     public function addUser($conn)
     {
         try {
             if ($this->validate()) {
-                $sql = 'insert into users(name, email, phone, address, password, is_active, role_id) values(:name, :email, :phone, :address, :password, :is_active, :role_id);';
+                $sql = 'insert into users(name, email, username, address, password, is_active, role_id) values(:name, :email, :username, :address, :password, :is_active, :role_id);';
                 $stmt = $conn->prepare($sql);
                 $stmt->bindValue(":name", $this->name, PDO::PARAM_STR);
                 $stmt->bindValue(":email", $this->email, PDO::PARAM_STR);
-                $stmt->bindValue(":phone", $this->phone, PDO::PARAM_STR);
+                $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
                 $stmt->bindValue(":address", $this->address, PDO::PARAM_STR);
                 $stmt->bindValue(":is_active", $this->is_active, PDO::PARAM_INT);
                 $stmt->bindValue(":role_id", $this->role_id, PDO::PARAM_INT);
