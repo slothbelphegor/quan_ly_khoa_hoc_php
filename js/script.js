bd45ca2d5c286338d4ac870baaba4f60d556162c
manle423 <trungmanle.vm@gmail.com>
2024-03-17 00:14:28 +0700

    update script

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         required: true,
         email: true,
       },
-      phone: "required",
+      username: "required",
       address: "required",
       password: "required",
       repassword: {
@@ -25,7 +25,7 @@ $(document).ready(function () {
         required: "Vui lòng nhập địa chỉ email",
         email: "Vui lòng nhập địa chỉ email hợp lệ",
       },
-      phone: "Vui lòng nhập số điện thoại của bạn",
+      username: "Vui lòng nhập tên người dùng",
       address: "Vui lòng nhập địa chỉ của bạn",
       password: "Vui lòng nhập mật khẩu",
       repassword: {
@@ -60,7 +60,7 @@ $(document).ready(function () {
         required: true,
         email: true,
       },
-      phone: "required",
+      username: "required",
       address: "required",
       password: "required",
       repassword: {
@@ -74,7 +74,7 @@ $(document).ready(function () {
         required: "Vui lòng nhập địa chỉ email",
         email: "Vui lòng nhập địa chỉ email hợp lệ",
       },
-      phone: "Vui lòng nhập số điện thoại của bạn",
+      username: "Vui lòng nhập tên người dùng",
       address: "Vui lòng nhập địa chỉ của bạn",
       password: "Vui lòng nhập mật khẩu",
       repassword: {
@@ -100,66 +100,6 @@ $(document).ready(function () {
     },
   });
 });
-/*
-$(document).ready(function () {
-  $("#frmLOGIN").validate({
-    rules: {
-      identifier: "required",
-      password: "required",
-    },
-    messages: {
-      identifier: "Vui lòng nhập email hoặc số điện thoại",
-      password: "Vui lòng nhập mật khẩu",
-    },
-    // submitHandler: function (form, event) {
-    //   event.preventDefault();
-    //   $.ajax({
-    //     url: "login.php",
-    //     type: "POST",
-    //     data: $(form).serialize(),
-    //     success: function (response) {
-    //       // $("#frmLOGIN").hide();
-    //       $(".success-message").text(response).show();
-    //       redirectToIndex();
-    //     },
-    //     error: function (jqXHR, textStatus, errorThrown) {
-    //       if (jqXHR.responseJSON && jqXHR.responseJSON.error) {
-    //         $(".error-message").text(jqXHR.responseJSON.error).show();
-    //       } else {
-    //         switch (jqXHR.status) {
-    //           case 401:
-    //             $(".error-message")
-    //               .text("Tài khoản hoặc mật khẩu không chính xác")
-    //               .show();
-    //             break;
-    //           default:
-    //             console.error("Lỗi:", textStatus, errorThrown);
-    //             $(".error-message").text("Lỗi không xác định").show();
-    //         }
-    //       }
-    //     },
-    //   });
-    // },
-  });
-});
-*/
-//Ham xu ly dang xuat
-// $(document).ready(function () {
-//   $("#logoutbtn").click(function () {
-//     if (window.confirm("Bạn có muốn đăng xuất?")) {
-//       $.ajax({
-//         url: "logout.php",
-//         method: "GET",
-//         success: function (response) {
-//           redirectToIndex();
-//         },
-//         error: function (xhr, status, error) {
-//           alert("Có lỗi xảy ra khi đăng xuất!");
-//         },
-//       });
-//     }
-//   });
-// });
 
 // hàm chuyển trang dăng nhập sau khi đăng ký thành công
 function redirectToLogin() {
@@ -225,7 +165,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  var addCourseBtn = document.getElementById("#addcoursebtn");
+  var addCourseBtn = document.getElementById("addcoursebtn");
   if (addCourseBtn) {
     addCourseBtn.addEventListener("click", function () {
       // Chuyển hướng người dùng đến trang addcourse.php
@@ -258,6 +198,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var btn = document.getElementById("changepassbtn");
+  if (btn) {
+    btn.addEventListener("click", function () {
+      window.location.href = "change_password.php";
+    });
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   var buttons = document.querySelectorAll("#btnDeleteCourse");
   buttons.forEach(function (button) {
@@ -303,47 +253,28 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   var form = document.getElementById('frmLOGIN');
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll("#btnDeactive");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var confirmDEL = confirm("SURE!?");
+      if (confirmDEL) {
+        var courseId = this.value;
+        window.location.href = "deactive_user.php?id=" + courseId;
+      }
+    });
+  });
+});
 
-//   form.addEventListener('submit', function (event) {
-//       event.preventDefault();
-
-//       var identifier = document.getElementById('identifier').value;
-//       var password = document.getElementById('password').value;
-
-//       if (identifier === '' || password === '') {
-//           // Hiển thị thông báo lỗi nếu trường email hoặc mật khẩu không được nhập
-//           document.querySelector('.error-message').textContent = "Vui lòng nhập email hoặc số điện thoại và mật khẩu";
-//           document.querySelector('.error-message').style.display = "block";
-//           return;
-//       }
-
-//       var xhr = new XMLHttpRequest();
-//       xhr.open('POST', 'login.php', true);
-//       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//       xhr.onreadystatechange = function () {
-//           if (xhr.readyState === XMLHttpRequest.DONE) {
-//               if (xhr.status === 200) {
-//                   // Xử lý khi nhận được phản hồi từ server
-//                   var response = xhr.responseText;
-//                   document.querySelector('.success-message').textContent = response;
-//                   document.querySelector('.success-message').style.display = "block";
-//                   redirectToIndex();
-//               } else {
-//                   // Xử lý khi có lỗi từ server
-//                   var errorMessage = "";
-//                   if (xhr.status === 401) {
-//                       errorMessage = "Tài khoản hoặc mật khẩu không chính xác";
-//                   } else {
-//                       errorMessage = "Lỗi không xác định";
-//                   }
-//                   document.querySelector('.error-message').textContent = errorMessage;
-//                   document.querySelector('.error-message').style.display = "block";
-//               }
-//           }
-//       };
-//       var formData = new FormData(form);
-//       xhr.send(new URLSearchParams(formData).toString());
-//   });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll("#btnActive");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var confirmDEL = confirm("SURE!?");
+      if (confirmDEL) {
+        var courseId = this.value;
+        window.location.href = "active_user.php?id=" + courseId;
+      }
+    });
+  });
+});
