3da3ca7f0ee43b478d300e70ffdfecc5b5e204f4
manle423 <trungmanle.vm@gmail.com>
2024-03-17 00:14:20 +0700

    add error handler

--- inc/init.php
+++ inc/init.php
@@ -11,3 +11,23 @@ spl_autoload_register(
 require dirname(__DIR__) . "/config.php";
 require dirname(__DIR__) . "/inc/function.php";
 if (session_id() === "") session_start();
+
+function errorHandler($level, $message, $file, $line)
+{
+    throw new ErrorException($message, 0, $level, $file, $line);
+}
+
+function exceptionHandler($ex)
+{
+    if (DEBUG) {
+        echo "<h2>Lỗi</h2>";
+        echo "<p>Exception: " . get_class($ex) . "</p>";
+        echo "<p>Nội dung: " . $ex->getMessage() . "</p>";
+        echo "<p>Tập Tin: " . $ex->getFile() . " dòng thứ: " . $ex->getLine() . "</p>";
+    } else {
+        echo "<h2>Lỗi, Vui lòng thử lại</h2>";
+    }
+}
+// Khi xảy ra lỗi thì dùng hàm của tôi
+set_error_handler('errorHandler');
+set_exception_handler('exceptionHandler');
