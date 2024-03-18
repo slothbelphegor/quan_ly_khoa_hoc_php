da3176556f7549db3f69288c29571b9b81f92203
sloth <andinhtran123@gmail.com>
2024-03-18 08:43:25 +0700

    Fixed footer quote

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quản lý khoá học</title>
-</head>
+
 <body>
 <section class="footer">
         
@@ -46,7 +40,7 @@
         
     </section>       
 </body>
-</html>
+
 <link rel="stylesheet" href="css/style2.css">   
 <marquee direction="left"><img src="images/flipped felix.gif" width="125"></marquee>
     
